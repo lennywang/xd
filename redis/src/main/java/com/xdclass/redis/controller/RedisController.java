747f8de99afc13550bf61217f4c680eecd06f851
wll <wllane@163.com>
2019-08-19 10:42:25 +0800

    redis

--- redis/src/main/java/com/xdclass/redis/controller/RedisController.java
+++ redis/src/main/java/com/xdclass/redis/controller/RedisController.java
@@ -1,12 +1,15 @@
 package com.xdclass.redis.controller;
 
 import com.xdclass.redis.RedisService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.lang.invoke.MethodHandles;
 
 /**
  *
@@ -14,6 +17,7 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping("/api/v1/redis")
 public class RedisController {
+    private Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     @Resource
     private RedisTemplate redisTemplate;
@@ -24,9 +28,11 @@ public class RedisController {
     @RequestMapping("/set_and_get")
     @ResponseBody
     public String setAndGet(String name,String key){
+        logger.info("开始执行setAndGet方法");
         redisTemplate.opsForValue().set(name,key);
         String result= (String) redisTemplate.opsForValue().get(name);
         System.out.println(result);
+        logger.info("结束执行setAndGet方法");
         return result;
     }
 
